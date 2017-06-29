282ffed856d70aa81dd700e1ccf53aede6b58956
zsy <zsy@zsy-2.local>
2017-06-29 10:07:22 +0800

    Initial Commit

---
+++ Mysegmentedcontrol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mysegmentedcontrol
+//
+//  Created by zsy on 2017/6/29.
+//  Copyright © 2017年 zsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
